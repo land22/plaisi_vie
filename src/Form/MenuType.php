99dab1dc916cf1b35c22589d95b848c41145e2f5
wabo landry <landrywabo8@gmail.com>
2020-01-26 16:13:42 +0100

    creation des formulaires avec la commande make:form

---
+++ src/Form/MenuType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Menu;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MenuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('boisson')
+            ->add('image')
+            ->add('nbr_point')
+            ->add('valide')
+            ->add('date_pub')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Menu::class,
+        ]);
+    }
+}
